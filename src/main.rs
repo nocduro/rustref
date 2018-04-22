14f660ec4b05d3d16d13f641018a5a28b723064d
Mackenzie Hauck <mhauck@live.ca>
2018-04-22 05:50:51 -0600

    add more tests, and verify URLs

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 
 extern crate cloudflare;
 extern crate dotenv;
+extern crate rayon;
 extern crate reqwest;
 extern crate rocket;
 extern crate rocket_contrib;
@@ -12,6 +13,7 @@ extern crate toml;
 
 use cloudflare::Cloudflare;
 use cloudflare::zones::dns;
+use rayon::prelude::*;
 use rocket::http::RawStr;
 use rocket::State;
 use rocket::response::Redirect;
@@ -27,7 +29,7 @@ type RedirectMap = RwLock<HashMap<String, String>>;
 type CloudflareApi = Mutex<Cloudflare>;
 
 mod errors;
-use errors::Result;
+use errors::{Error, RedirectError, Result};
 
 #[derive(Debug, Deserialize, Clone, PartialEq, Eq)]
 struct GithubUserShort {
@@ -76,10 +78,41 @@ struct SiteRedirect {
     url: String,
 }
 
-fn verify_redirects(redirs: &[SiteRedirect]) -> Result<()> {
-    // return the redirects that faild...
+fn verify_redirects(redirects: &mut [SiteRedirect]) -> Result<()> {
+    // verify that we have no duplicate redirect rules
+    redirects.sort();
+    let mut errors: Vec<RedirectError> = redirects
+        .windows(2)
+        .filter(|w| w[0].short == w[1].short)
+        .map(|w| RedirectError::DuplicateRule(w[0].short.clone()))
+        .collect();
 
-    Ok(())
+    // verify URLs are valid syntactically, and that the URL is online
+    errors.extend(
+        redirects
+            .par_iter()
+            .filter_map(|x| check_url(&x.url).err())
+            .collect::<Vec<RedirectError>>(),
+    );
+
+    if errors.len() > 0 {
+        Err(Error::RedirectErrors(errors))
+    } else {
+        Ok(())
+    }
+}
+
+/// Verify that `url` is syntactically valid, and that the page is reachable
+fn check_url(url: &str) -> std::result::Result<(), RedirectError> {
+    let resp = reqwest::get(url).map_err(|e| RedirectError::BadUrl(url.to_string()))?;
+    match resp.status().is_success() {
+        true => Ok(()),
+        false => Err(RedirectError::InvalidPage(format!(
+            "{}: {}",
+            url,
+            resp.status()
+        ))),
+    }
 }
 
 fn update_redirect_map(redirs: State<RedirectMap>, cf: State<CloudflareApi>) -> Result<()> {
@@ -88,8 +121,8 @@ fn update_redirect_map(redirs: State<RedirectMap>, cf: State<CloudflareApi>) -> 
     let toml_str = reqwest::get(
         "https://raw.githubusercontent.com/nocduro/rustref/master/redirects.toml",
     )?.text()?;
-    let new_redirects = toml::from_str::<TomlConfig>(&toml_str)?.redirect;
-    verify_redirects(&new_redirects)?;
+    let mut new_redirects = toml::from_str::<TomlConfig>(&toml_str)?.redirect;
+    verify_redirects(&mut new_redirects)?;
     println!("toml: {}", &toml_str);
 
     // before setting the new redirects, make sure that cloudflare was updated successfully
@@ -175,10 +208,7 @@ fn main() {
     ).expect("failed to create cloudflare client");
 
     rocket::ignite()
-        .mount(
-            "/",
-            routes![index, redirect, redirect_bare, webhook],
-        )
+        .mount("/", routes![index, redirect, redirect_bare, webhook])
         .manage(RwLock::new(redirects))
         .manage(Mutex::new(cf_api))
         .launch();
@@ -213,5 +243,82 @@ mod tests {
             assert!(redir.short.len() > 0);
             assert!(redir.url.len() > 0);
         }
+    }
+
+    #[test]
+    fn verify_production_redirects_valid() {
+        let toml_str = include_str!("../redirects.toml");
+        let parsed = toml::from_str::<TomlConfig>(&toml_str);
+        assert!(parsed.is_ok());
+        let mut redir_vec = parsed.unwrap().redirect;
+        match verify_redirects(&mut redir_vec) {
+            Ok(_) => (),
+            Err(Error::RedirectErrors(e)) => {
+                let fail_str: String = e.iter().map(|f| format!("{:?}\n", f)).collect();
+                panic!(fail_str);
+            },
+            Err(e) => panic!("invalid redirect error: {:?}", e),
+        }
+    }
+
+    #[test]
+    fn malformed_urls() {
+        let bad1 = SiteRedirect{short: "bad1".to_string(), url: "@#hello/test".to_string()};
+        let bad2 = SiteRedirect{short: "bad2".to_string(), url: "/example.com".to_string()};
+        let bad3 = SiteRedirect{short: "bad3".to_string(), url: "http://example".to_string()};
+        let bad4 = SiteRedirect{short: "bad4".to_string(), url: "test".to_string()};
+        let mut vector = vec![bad1, bad2, bad3, bad4];
+        match verify_redirects(&mut vector) {
+            Ok(_) => panic!("should fail"),
+            Err(Error::RedirectErrors(e)) => {
+                if e.len() == vector.len() {
+                    return;
+                }
+                let mut fail_str = String::from("only these URLs failed\n");
+                for fail in e {
+                    fail_str.push_str(&format!("{:?}", fail));
+                    fail_str.push('\n');
+                }
+                panic!(fail_str);
+            },
+            Err(e) => panic!("unexpected error: {:?}", e),
+        }
+    }
+
+    #[test]
+    fn test_duplicate_redirects() {
+        let bad1 = SiteRedirect{short: "same".to_string(), url: "https://nocduro.com".to_string()};
+        let bad2 = SiteRedirect{short: "same".to_string(), url: "https://google.com".to_string()};
+        let bad3 = SiteRedirect{short: "bad2".to_string(), url: "https://google.com".to_string()};
+        let mut vector = vec![bad1, bad2, bad3];
+        match verify_redirects(&mut vector) {
+            Ok(_) => panic!("unexpected pass"),
+            Err(Error::RedirectErrors(e)) => {
+                if e.len() == 1 {
+                    return;
+                }
+                panic!("Expected 1 failure for the duplicate");
+            },
+            Err(e) => panic!("unexpected error: {:?}", e),
+        }
+    }
+
+    #[test]
+    fn check_url_404() {
+        assert!(check_url("https://nocduro.com/invalid_page_name").is_err());
+    }
+
+    #[test]
+    #[ignore]
+    /// this url is valid for some reason!?
+    fn check_url_misspell() {
+        assert!(check_url("htp://nocduro.com").is_err())
+    }
+
+    #[test]
+    fn check_url_valid() {
+        assert!(check_url("https://nocduro.com/").is_ok());
+        assert!(check_url("https://doc.rust-lang.org/").is_ok());
+        assert!(check_url("https://doc.rust-lang.org").is_ok());
     }
 }
